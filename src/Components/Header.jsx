6604c5f685d0cab68680f78ed0bb1509e129d049
Anggi Permana <bagasrizki700@gmail.com>
2023-05-29 20:37:29 +0700

    lasts

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
-let nama = "Anggi Permana";
+let nama = "Bagas Rizky";
 function Header() {
   return <header>{nama}</header>;
 }
