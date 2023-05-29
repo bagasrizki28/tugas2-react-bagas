ebab4059c747bfd0b0394680f9235ef93251729f
Anggi Permana <bagasrizki700@gmail.com>
2023-05-29 20:36:08 +0700

    last

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+let nama = "Anggi Permana";
+function Header() {
+  return <header>{nama}</header>;
+}
+
+export default Header;
---
+++ src/Components/Login.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Tombol from "./Tombol";
+import KotakTeks from "./KotakTeks";
+
+function Login() {
+  return (
+    <>
+      <KotakTeks teks="Email" />
+      <KotakTeks teks="Password" />
+      <Tombol teks="Login" />
+    </>
+  );
+}
+
+export default Login;
